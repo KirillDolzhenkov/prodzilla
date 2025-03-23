03f3d527f42205505fb5208c91fc14250a136be8
KirillDolzhenkov <k.dolzhenkov@gmail.com>
2025-03-24 02:15:30 +0300

    webpack init

---
+++ src/test.js
@@ -0,0 +1,5 @@
+export function someFn(arg) {
+    console.log('test')
+
+    return '';
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src', 'index.js'),
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        }),
+        new webpack.ProgressPlugin(),
+    ],
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.tsx?$/,
+    //             use: 'ts-loader',
+    //             exclude: /node_modules/,
+    //         }
+    //     ]
+    // },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    }
+}
