5ab01f76384a6b1685977c6f8835670335adf913
kirilldolzhenkov <k.dolzhenkov@gmail.com>
2026-02-16 03:23:06 +0300

    featL config decomposition

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,36 +1,22 @@
-import path from 'path';
+import path    from 'path';
 import webpack from 'webpack';
-import HTMLWebpackPlugin from "html-webpack-plugin";
 
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildPaths }         from './config/build/types/config';
 
-const config: webpack.Configuration = {
-    mode: 'development',
-    entry: {
-        main: path.resolve(__dirname, 'src', 'index.ts'),
-    },
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'build'),
-        clean: true,
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html'),
-        }),
-        new webpack.ProgressPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    }
-}
+const paths: BuildPaths = {
+  build: path.resolve(__dirname, 'build'),
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  html:  path.resolve(__dirname, 'public', 'index.html'),
+};
+
+const mode  = 'development';
+const isDev = mode === 'development';
+
+const config: webpack.Configuration = buildWebpackConfig({
+  isDev,
+  mode: 'development',
+  paths,
+});
 
 export default config;
